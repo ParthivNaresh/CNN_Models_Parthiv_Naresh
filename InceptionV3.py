8ec691db0d8bc223382471c3f972fc17377baf56
NutellaAndCurry <parthivnaresh@gmail.com>
2019-08-19 17:20:06 -0400

    Changed file name to reflect model being used

--- InceptionV3.py
+++ InceptionV3.py
@@ -43,27 +43,3 @@ def list_layers():
         print(class_of_layer[class_of_layer.rfind('.') + 1 : ] + " - " + layer.name)
 
 a = list_layers()
-
-'''
-model = tf.keras.models.Sequential([
-        # The input shape is the desired size of the image 150x150 with 3 bytes color
-        # This is the first convolution
-        tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        # The second convolution
-        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2,2),
-        # The third convolution
-        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2,2),
-        # The fourth convolution
-        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2,2),
-        # Flatten the results to feed into a DNN
-        tf.keras.layers.Flatten(),
-        #tf.keras.layers.Dropout(0.5),
-        # 512 neuron hidden layer
-        tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(30, activation='softmax')])
-model.summary()
-'''
